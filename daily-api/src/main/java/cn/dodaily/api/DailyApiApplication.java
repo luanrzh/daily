b73c6b3119e0a152440e2089db12f1d3bbf5e1f7
luanrzh <34942224+luanrzh@users.noreply.github.com>
2019-08-06 17:54:18 +0800

    Update and rename DailyServerApplication.java to DailyApiApplication.java

--- daily-api/src/main/java/cn/dodaily/api/DailyApiApplication.java
+++ daily-api/src/main/java/cn/dodaily/api/DailyApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DailyServerApplication {
+public class DailyApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DailyServerApplication.class, args);
+        SpringApplication.run(DailyApiApplication.class, args);
     }
 
 }
