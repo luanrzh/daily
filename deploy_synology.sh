58c6a0dd5c679e56b7a6cc8724af38d47a879d81
luanrzh <luanrzh@gmail.com>
2020-04-07 11:34:49 +0800

    update deploy_synology.sh

--- deploy_synology.sh
+++ deploy_synology.sh
@@ -23,14 +23,14 @@ git pull origin master
 
 echo -e "\n----------构建部署daily-front----------"
 cd ${CODE_DIR_FE}
-sed -i "s/api.dodaily.cn/192.168.1.105:8000/" src/api/api.js 
+sed -i "s/api.dodaily.cn/localhost:8000/" src/api/api.js 
 npm run build
 rm -rf ${DEPOLY_FRONTEND_DIR}
 mv ${CODE_DIR_FE}/dist $DEPOLY_FRONTEND_DIR
 
 echo -e "\n----------构建部署daily-api----------"
 cd ${CODE_DIR_API}
-# sed -i "s/www.dodaily.cn/192.168.1.105/" src/main/resources/application.yml
+# sed -i "s/www.dodaily.cn/localhost/" src/main/resources/application.yml
 mvn clean package -DskipTests
 mv target/daily-api-0.0.1.jar ${DEPOLY_FRONTEND_DIR}
 
@@ -41,6 +41,6 @@ if [ -n "$JAR_PID" ]; then
     kill -9 $JAR_PID
 fi
 #启动服务
-nohup java -jar ${DEPOLY_FRONTEND_DIR}/daily-api-0.0.1.jar >>${DEPOLY_FRONTEND_DIR}/daily_api_nohup.log &
+nohup java -jar ${DEPOLY_FRONTEND_DIR}/daily-api-0.0.1.jar --server.port=8000 >>${DEPOLY_FRONTEND_DIR}/daily_api_nohup.log &
 
 echo -e "\n----------ALL DONE!----------"
